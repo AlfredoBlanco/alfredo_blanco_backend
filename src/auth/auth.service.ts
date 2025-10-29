0512e417dbb9ea75ffa0071706157398431fbf9f
Alfredo Blanco <alfre.blanco@icloud.com>
2025-10-29 16:13:37 -0300

    Initial commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+
+    async registerUser() {
+        return { json: 'Heeelo' }
+    }
+    async getHello() {
+        return { json: 'Heeelo' }
+    }
+}
